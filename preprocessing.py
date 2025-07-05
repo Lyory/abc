b0e6526fc1d7a6311607df9ca1ebd46b3920ef13
Lyory <165302873+Lyory@users.noreply.github.com>
2025-07-05 20:03:39 +0700

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,21 @@
-# Quân
-
 import numpy as np
-import cv2 as cv
-import glob
+import cv2
 
-img = glob.glob('samples/ArUco.jpg')
+def maximize_contrast_clahe(img_gray):
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    return clahe.apply(img_gray)
+
+def preprocess_image(img_input):
+    img_gray = cv2.cvtColor(img_input, cv2.COLOR_BGR2GRAY)
+    img_contrast = maximize_contrast_clahe(img_gray)
+    img_blurred = cv2.GaussianBlur(img_contrast, (5, 5), 0)
+    _, img_binary = cv2.threshold(img_blurred, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    return img_binary
+
+img = cv2.imread('samples/2.jpg')  
+img_result = preprocess_image(img)
+
+cv2.imshow('Original', img)
+cv2.imshow('Preprocessed', img_result)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
